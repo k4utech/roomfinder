861b9b520add9bea9ffbeaa7a2a4a8c302b4d6ef
Kishor <k4utech@gmail.com>
2018-05-27 21:26:53 +0530

    spring boot server and static ui set up

---
+++ src/main/java/com/k4utech/roomfinder/RoomfinderApplication.java
@@ -0,0 +1,12 @@
+package com.k4utech.roomfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomfinderApplication.class, args);
+	}
+}
