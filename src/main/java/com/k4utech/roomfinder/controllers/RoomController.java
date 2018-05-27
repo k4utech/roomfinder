2bab3d4774bdb72b2344ef1f5a5e53aca5206a05
Kishor <k4utech@gmail.com>
2018-05-27 22:44:06 +0530

    plugged controllers with caches

--- src/main/java/com/k4utech/roomfinder/controllers/RoomController.java
+++ src/main/java/com/k4utech/roomfinder/controllers/RoomController.java
@@ -1,6 +1,7 @@
 package com.k4utech.roomfinder.controllers;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.websocket.server.PathParam;
@@ -13,25 +14,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.k4utech.roomfinder.cache.RoomCache;
 import com.k4utech.roomfinder.models.Room;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
-	
+
 	@GetMapping
-	public List<Room> list(){
-		return new ArrayList<Room>();
+	public Collection<Room> list(){
+		return RoomCache.getAll();
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@RequestBody Room room) {
-		
+		RoomCache.save(room);
 	}
 	
 	@GetMapping("/{id}")
 	public Room get(@PathParam("id") int id) {
-		return new Room();
+		return RoomCache.getOne(id);
 	}
 }
--- src/main/java/com/k4utech/roomfinder/controllers/RoomStatusController.java
+++ src/main/java/com/k4utech/roomfinder/controllers/RoomStatusController.java
@@ -1,7 +1,8 @@
 package com.k4utech.roomfinder.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.k4utech.roomfinder.cache.RoomStatusCache;
 import com.k4utech.roomfinder.models.RoomStatus;
 
 @RestController
@@ -18,17 +20,20 @@ import com.k4utech.roomfinder.models.RoomStatus;
 public class RoomStatusController {
 	
 	@GetMapping
-	public List<RoomStatus> list() {
-		return new ArrayList<>();
+	public Collection<RoomStatus> list() {
+		return RoomStatusCache.getAll();
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@RequestBody RoomStatus status) {
-		
+		RoomStatusCache.save(status);
 	}
 	
-	
+	@GetMapping("/{id}")
+	public RoomStatus get(@PathParam("id") int id) {
+		return RoomStatusCache.getOne(id);
+	}
 	
 	
 }
